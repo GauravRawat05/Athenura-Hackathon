266fe65d1037c0c4e00663557f0fa714848763c2
bimalgautam1 <gbimal3210@gmail.com>
2026-05-01 20:41:04 +0545

    Refactor authentication module: - Moved authentication logic to auth.controller.js, auth.service.js, and auth.repository.js. - Implemented user registration, login, verification, and session management. - Added validation for user inputs using Joi. - Updated routes to use asyncHandler for error handling. - Removed unused user.controller.js and related utility files. - Introduced envConfig for environment variable management. - Enhanced user model with role determination and token generation methods. -  Made the codebase more modular

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -4,10 +4,10 @@
  */
 //jwt is used to verify the token
 import User from "../modules/users/user.model.js"
-import ApiError from "../utils/ApiError.js"
+import ApiError from "../libs/apiError.js"
 import jwt from "jsonwebtoken"
-import { accessTokenSecret } from "../utils/config.js"
-import asyncHandler from '../utils/asyncHandler.js';
+import envConfig from "../config/envConfig.js"
+import asyncHandler from '../libs/asyncHandler.js';
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
   const token =
@@ -19,7 +19,7 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
     throw new ApiError(401, "Unauthorized request")
   }
 
-  const decodedToken = jwt.verify(token, accessTokenSecret)
+  const decodedToken = jwt.verify(token, envConfig.accessTokenSecret)
 
   const user = await User.findById(decodedToken._id).select(
     "-password -refreshToken"
@@ -46,7 +46,7 @@ export const verifyAdmin = asyncHandler(async (req, res, next) => {
       throw new ApiError(401, "Unauthorized request");
     }
 
-    const decodedToken = jwt.verify(token, accessTokenSecret);
+    const decodedToken = jwt.verify(token, envConfig.accessTokenSecret);
     const user = await User.findById(decodedToken._id).select("-password -refreshToken");
 
     if (!user) {
