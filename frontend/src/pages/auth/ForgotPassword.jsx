e9b7caa2b869255c7a78db4871594e56218169d6
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-05-12 17:49:50 +0530

    Added hero section and Filters page

--- frontend/src/pages/auth/ForgotPassword.jsx
+++
@@ -1,3 +0,0 @@
-﻿export default function ForgotPassword() {
-  return <div className="p-8 text-2xl font-bold">ForgotPassword Page</div>
-}
--- frontend/src/pages/auth/Login.jsx
+++
@@ -1,3 +0,0 @@
-﻿export default function Login() {
-  return <div className="p-8 text-2xl font-bold">Login Page</div>
-}
